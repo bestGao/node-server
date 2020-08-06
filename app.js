b8a4a70d5b71853dac6fb2050c62227c2765ae81
gao <1055652678@qq.com>
2020-08-06 21:00:46 +0800

    fix

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ const WebSocket = require("ws");
 const WebSocketServer = WebSocket.Server;
 
 const wss = new WebSocketServer({
-  port: 9966, // 9898
+  port: 9898
 });
 
 function fetchIndexes (callback) {
   const curHours = new Date().getHours();
   if (curHours > 15) {
+    console.log('成功')
     // 下午三点后模拟数据
     mockArray.forEach(function (dItem) {
       if (Math.random() > 0.5) {
@@ -23,6 +24,7 @@ function fetchIndexes (callback) {
         dItem.f4 = dItem.f4 + Math.random() * 2.5
       }
     })
+    callback(mockArray)
   } else {
     const url =
       `https://push2.eastmoney.com/api/qt/ulist.np/get?fltt=2&fields=f2,f3,f4,f12,f14&secids=${indexCodes.join(',')}&_=` +
@@ -37,7 +39,7 @@ let intervalId;
 let tempData;
 wss.on("connection", function (ws) {
   ws.on("message", function (mesg) {
-    // console.log("客户端传来的数据：", mesg);
+    console.log("客户端传来的数据：", mesg);
     if (mesg === "jayGao") {
       // 暗号正确
       intervalId = setInterval(function () {
